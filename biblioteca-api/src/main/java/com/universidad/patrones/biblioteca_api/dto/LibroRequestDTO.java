c097dd45c05c4c1b108cb28cfce2ef766b55f464
JahirBautistaQ <02210132008@mail.udes.edu.co>
2026-04-19 21:46:32 -0500

    Paso 3 completado: DTOs y Mapper funcionando

---
+++ biblioteca-api/src/main/java/com/universidad/patrones/biblioteca_api/dto/LibroRequestDTO.java
@@ -0,0 +1,29 @@
+package com.universidad.patrones.biblioteca_api.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LibroRequestDTO {
+
+    @NotBlank(message = "El título es obligatorio")
+    private String titulo;
+
+    @NotBlank(message = "El autor es obligatorio")
+    private String autor;
+
+    @NotBlank(message = "El ISBN es obligatorio")
+    private String isbn;
+
+    @Min(1800)
+    @Max(2100)
+    private Integer anioPublicacion;
+
+    private String categoria;
+}
